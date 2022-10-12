0d3aff4fdcc488d66203fb8f8259775064355606
Ramalh0z <arthu.ramalh0z@gmail.com>
2022-10-12 15:28:31 -0300

    first image!

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+const int IMAGE_WIDTH  = 256;
+const int IMAGE_HEIGHT = 256;
+
+int main()
+{
+    std::cout << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << "\n255\n";
+
+    // Render loop to image
+    for (int j = IMAGE_HEIGHT-1; j >= 0; --j) {
+        for (int i = 0; i < IMAGE_WIDTH; ++i) {
+
+            float r = float(i) / (IMAGE_WIDTH-1);
+            float g = float(j) / (IMAGE_HEIGHT-1);
+            float b = 0.25;
+
+            int image_r = static_cast<int>(255.999 * r);
+            int image_g = static_cast<int>(255.999 * g);
+            int image_b = static_cast<int>(255.999 * b);
+
+            std::cout << image_r << ' ' << image_g << ' ' << image_b << '\n';
+        }
+    }
+}
